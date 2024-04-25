b54fed2624473e285b2eb76b020fd807583e68fb
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-04-25 16:15:36 +0300

    add marcup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { GetDataService } from '../services/get-data.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+
+
+export class HomeComponent implements OnInit {
+  bannerImgs = [
+    {
+      id: 1,
+      img: '../../assets/images/banner/ef637eb93bf1a887.webp',
+    },
+    {
+      id: 2,
+      img: '../../assets/images/banner/9021283f0be266c1.webp',
+    },
+    {
+      id: 3,
+      img: '../../assets/images/banner/7dcc28ed89760319.webp',
+    },
+  ];
+
+  getCategoriesData: any;
+  getApplianceProductData: any = [];
+  getFashionProductData: any = [];
+
+  constructor(private getData: GetDataService) { }
+  ngOnInit(): void {
+    this.getCategoriesData = this.getData.categoriesData
+  }
 
 }
