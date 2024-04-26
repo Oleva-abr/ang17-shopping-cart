20089a5e2d6db2742d0d06969d9c301a80e3c89f
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-04-27 00:16:15 +0300

    product-page

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { GetDataService } from '../services/get-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule, RouterModule],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
 
+  getParamValue: any;
+  getProductData: any = [];
+  filterProductData: any = [];
+  getSubCategoryOption: any = [];
+  constructor(private route: ActivatedRoute, private getData: GetDataService) { }
+
+  ngOnInit(): void {
+    this.getParamValue = this.route.snapshot.paramMap.get('name');
+
+    this.getData.productData.filter((el: any) => {
+      if (el.pdCategory == this.getParamValue) {
+        this.getProductData.push(el);
+        this.filterProductData.push(el);
+      }
+    });
+
+    this.getData.subCategorisFilterData.filter((el: any) => {
+      if (el.categories == this.getParamValue) {
+        this.getSubCategoryOption.push(el);
+      }
+    });
+  }
+
+  filterSelect(data: any) {
+    this.filterProductData = [];
+    var getFilterValue: any = data.target.value;
+    console.log(getFilterValue, 'getFilterValue');
+
+    if (getFilterValue != 'all') {
+      this.getData.productData.filter((el: any) => {
+        if (el.pdSubCategory == getFilterValue) {
+          this.filterProductData.push(el);
+        }
+      });
+    } else {
+      this.filterProductData = this.getProductData;
+    }
+  }
 }
