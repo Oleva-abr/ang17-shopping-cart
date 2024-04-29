5f53e928d0683713ac43d32e5f617177ea062748
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-04-30 00:03:34 +0300

    add cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { DataStorageService } from '../services/data-storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  constructor(private dataStorage: DataStorageService) { }
+
+  getCartData: any;
+  storeCartArray: any = [];
+  totalAmount: number = 0;
+  totalCart: number = 0;
+
+
+  ngOnInit(): void {
+    this.getCartData = this.dataStorage.getCartData();
+    this.totalCart = this.getCartData.length;
+    this.getCartData.filter((el: any) => {
+      this.totalAmount = el.pdPrice + this.totalAmount
+    })
+  }
+
+
+  removeCart(data: any) {
+    this.totalAmount = 0;
+    localStorage.removeItem("cart-data")
+    this.storeCartArray = [];
+    this.getCartData.filter((el: any) => {
+      if (el.pdId != data.pdId) {
+        this.storeCartArray.push(el);
+        this.totalAmount = el.pdPrice + this.totalAmount;
+      }
+    })
+    this.dataStorage.storeCartData(this.storeCartArray);
+    this.getCartData = this.dataStorage.getCartData();
+    this.totalCart = this.getCartData.length;
+  }
 }
